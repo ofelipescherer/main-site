d2ee615f2536c71ff4a83b6d09410fc7db042de2
Felipe Scherer <felipescherer25@gmail.com>
2022-12-13 13:11:39 -0300

    :bug: fix: lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -8,21 +8,3 @@ const buildEslintCommand = (filenames) =>
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand]
 }
-
-// module.exports = {
-//   // // Type check TypeScript files
-//   // '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
-
-//   // // Lint & Prettify TS and JS files
-//   // '**/*.(ts|tsx|js)': (filenames) => [
-//   //   `npx eslint ${filenames.join(' ')}`,
-//   //   `npx prettier --write ${filenames.join(' ')}`
-//   // ],
-
-//   // // Prettify only Markdown and JSON files
-//   // '**/*.(md|json)': (filenames) =>
-//   //   `npx prettier --write ${filenames.join(' ')}`,
-
-//   '**/*': ['npm run lint --fix'],
-//   '*.js': 'eslint --cache --fix'
-// }
